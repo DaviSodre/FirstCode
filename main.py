1d463b843bb75ce56d93b96fd9b06bba04590450
Davi Sodré <124099906+DaviSodre@users.noreply.github.com>
2023-01-31 20:44:51 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int(input('Digite sua idade: '))
+print(f'Olá, {nome}, você tem {idade} anos.')
+print(type(idade))
+print(f'Sua idade daqui a 10 anos será {idade+10}')
